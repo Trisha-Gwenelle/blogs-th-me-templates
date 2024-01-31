f118aeae87bb7f3da1e63f7412dd13a9740174c6
Trisha <trisha@bignon.koi>
2024-01-31 13:30:22 +0100

    Ajout de la boucle pour terminer ou recommencer le programme

--- hello.py
+++ hello.py
@@ -4,3 +4,12 @@ print("Vous avez saisi le nombre ", user_input)
 user_number = int(user_input)
 for i in range(1, 10):
     print(f"{user_number} x {i} = {user_number * i}")
+while True:
+    user_input = input("Veuillez entrer un nombre : ")
+    print("Vous avez entré le nombre :", user-input)
+    user_number = int(user_input)
+    for i in range(1, 10):
+        print(f"{user_number} x {i} = {user_number * i}")
+    response = input("Voulez-vous finir  le programme ? (Oui/Non) : ")
+    if response.lower() == "oui":
+        break
