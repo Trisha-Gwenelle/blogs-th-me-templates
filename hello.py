898d5d275af62e360c621865992d93d0e1a9d408
Trisha <trisha@bignon.koi>
2024-01-31 13:22:23 +0100

    Affichage des multiples de 1 à 9 du nombre saisi

--- hello.py
+++ hello.py
@@ -1,3 +1,6 @@
 print("hey, guy!")
-user_imput = imput("3")
-print("Vous avez saisi le nombre ", user_imput)
+user_input = input("3")
+print("Vous avez saisi le nombre ", user_input)
+user_number = int(user_input)
+for i in range(1, 10):
+    print(f"{user_number} x {i} = {user_number * i}")
